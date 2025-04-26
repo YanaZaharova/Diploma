5252f25196c18ffe5919ed3ab1bf0534ee5ee214
Zaharova Yana <yanamelomano@mail.ru>
2025-04-26 14:29:39 +0300

    Added Helpers

--- src/test/java/ru/netology/data/DataHelper.java
+++ src/test/java/ru/netology/data/DataHelper.java
@@ -2,6 +2,8 @@ package ru.netology.data;
 
 import com.github.javafaker.Faker;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
 public class DataHelper {
@@ -27,59 +29,99 @@ public class DataHelper {
         return "DECLINED";
     }
 
-    public static String getValidMonth() {}
+    public static int getValidMonth() {
+        return LocalDate.now().getMonthValue();
+    }
 
-    public static String getValidYear() {}
+    public static String getValidYear() {
+        return LocalDate.now().plusYears(9).format(DateTimeFormatter.ofPattern("yy"));
+    }
 
-    public static String getValidOwner() {}
+    public static String getValidOwner() {
+        return engFaker.name().firstName() + " " + engFaker.name().lastName();
+    }
 
-    public static String getValidCode() {}
+    public static String getValidCode() {
+        return rusFaker.number().digits(3);
+    }
 
     public static String getEmptyCardNumber() {
         return "";
     }
 
     public static String getOneSymbolCard() {
-
+        return rusFaker.number().digits(1);
     }
 
     public static String get15SymbolCard() {
-
+        return rusFaker.number().digits(15);
     }
 
     public static String getRandomCardNumber() {
         return rusFaker.business().creditCardNumber();
     }
 
-    public static String getOneSymbolInMonth() {}
+    public static String getOneSymbolInMonth() {
+        return rusFaker.number().digits(1);
+    }
 
-    public static String get13InMonth() {}
+    public static String get13InMonth() {
+        return "13";
+    }
 
-    public static String getZerosInMonth() {}
+    public static String getZerosInMonth() {
+        return "00";
+    }
 
-    public static String getEmptyMonth() {}
+    public static String getEmptyMonth() {
+        return "";
+    }
 
-    public static String getOneSymbolInYear() {}
+    public static String getOneSymbolInYear() {
+        return rusFaker.number().digits(1);
+    }
 
-    public static String get11YearsMoreThanCurrentYear() {}
+    public static String get11YearsMoreThanCurrentYear() {
+        return LocalDate.now().plusYears(11).format(DateTimeFormatter.ofPattern("yy"));
+    }
 
-    public static String getMaximumTermInYear() {}
+    public static String getMaximumTermInYear() {
+        return LocalDate.now().plusYears(10).format(DateTimeFormatter.ofPattern("yy"));
+    }
 
-    public static String getPresentYear() {}
+    public static String getPresentYear() {
+        return LocalDate.now().format(DateTimeFormatter.ofPattern("yy"));
+    }
 
-    public static String getPreviousYear() {}
+    public static String getPreviousYear() {
+        return LocalDate.now().minusYears(1).format(DateTimeFormatter.ofPattern("yy"));
+    }
 
-    public static String getEmptyYear() {}
+    public static String getEmptyYear() {
+        return "";
+    }
 
-    public static String getCyrillicSymbolsInOwner() {}
+    public static String getCyrillicSymbolsInOwner() {
+        return rusFaker.name().firstName() + " " + rusFaker.name().lastName();
+    }
 
-    public static String getNumbersInOwner() {}
+    public static int getNumbersInOwner() {
+        return rusFaker.number().randomDigit();
+    }
 
-    public static String getDiacriticsInOwner() {}
+    public static String getDiacriticsInOwner() {
+        return "$*@#&^";
+    }
 
-    public static String getSpacesInOwner() {}
+    public static String getSpacesInOwner() {
+        return "         ";
+    }
 
-    public static String getCodeWithTwoNumbers() {}
+    public static String getCodeWithTwoNumbers() {
+        return rusFaker.number().digits(2);
+    }
 
-    public static String getEmptyCode() {}
+    public static String getEmptyCode() {
+        return "";
+    }
 }
--- src/test/java/ru/netology/data/SQLHelper.java
+++ src/test/java/ru/netology/data/SQLHelper.java
@@ -1,6 +1,8 @@
 package ru.netology.data;
 
+import lombok.SneakyThrows;
 import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.ScalarHandler;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -15,5 +17,35 @@ public class SQLHelper {
 
     private static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(System.getProperty("db.url"), "app", "pass");
+    }
+
+    @SneakyThrows
+    public static void cleanAllData() {
+        var request = "DELETE FROM credit_request_entity;";
+        var order = "DELETE FROM order_entity;";
+        var payment = "DELETE FROM payment_entity;";
+        try (var conn = getConnection()) {
+            runner.update(conn, request);
+            runner.update(conn, order);
+            runner.update(conn, payment);
+        }
+    }
+
+    @SneakyThrows
+    public static String getRequestStatusCardPayment() {
+        QueryRunner runner = new QueryRunner();
+        String SQLRequest = "SELECT status FROM payment_entity ORDER BY created DESC LIMIT 1";
+        try (var conn = getConnection()) {
+            return runner.query(conn, SQLRequest, new ScalarHandler<>());
+        }
+    }
+
+    @SneakyThrows
+    public static String getRequestStatusCreditPayment() {
+        QueryRunner runner = new QueryRunner();
+        String SQLRequest = "SELECT status FROM credit_request_entity ORDER BY created DESC LIMIT 1";
+        try (var conn = getConnection()) {
+            return runner.query(conn, SQLRequest, new ScalarHandler<>());
+        }
     }
 }
